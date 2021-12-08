d73dfa635b5c7ac0c110375407326233c8491eef
A0321I1-NguyenNgocLePhuc <phucnnlde140045@fpt.edu.vn>
2021-12-08 18:19:22 +0700

    12,13,14

--- case_study_sql/database.sql
+++ case_study_sql/database.sql
@@ -220,9 +220,7 @@ where (ho_ten like 'H%') or  (ho_ten like 'T%') or (ho_ten like 'K%')
 having soluongkituten<=15;
 
 -- 3.	Hiển thị thông tin của tất cả khách hàng có độ tuổi từ 18 đến 50 tuổi và có địa chỉ ở “Đà Nẵng” hoặc “Quảng Trị”.
-select * 
-from khach_hang 
-where (51*30*365) > (curdate()-ngay_sinh) and (curdate()-ngay_sinh) >(18*30*365) and (dia_chi like '%Đà Nẵng%' or dia_chi like '%Quảng Trị%');
+SELECT *  from khach_hang where DATE_FORMAT(FROM_DAYS(DATEDIFF(now(),khach_hang.ngay_sinh)), '%Y') between 18 and 50 and (dia_chi like '%Đà Nẵng%' or dia_chi like '%Quảng Trị%');
 -- 4.	Đếm xem tương ứng với mỗi khách hàng đã từng đặt phòng bao nhiêu lần.
 -- Kết quả hiển thị được sắp xếp tăng dần theo số lần đặt phòng của khách hàng. 
 -- Chỉ đếm những khách hàng nào có Tên loại khách hàng là “Diamond”.
@@ -231,9 +229,9 @@ group by khach_hang.ma_khach_hang order by so_lan_dat_phong;
 
 -- 5.	Hiển thị ma_khach_hang, ho_ten, ten_loai_khach, ma_hop_dong, ten_dich_vu, ngay_lam_hop_dong, ngay_ket_thuc, tong_tien 
 -- (Với tổng tiền được tính theo công thức như sau: Chi Phí Thuê + Số Lượng * Giá, với Số Lượng và Giá là từ bảng dich_vu_di_kem, hop_dong_chi_tiet) 
--- cho tất cả các khách hàng đã từng đặt phòng. (những khách hàng nào chưa từng đặt phòng cũng phải hiển thị ra).
+-- cho tất cả các khách hàng đã từng đặt phòng. (những khách hàng nào chưa từng đặt phòng cũng phải hiển thị ra). Còn bugg tổng tiền khác đáp án file exel
 select khach_hang.ma_khach_hang, khach_hang.ho_ten, loai_khach.ten_loai_khach, hop_dong.ma_hop_dong, dich_vu.ten_dich_vu, hop_dong.ngay_lam_hop_dong, hop_dong.ngay_ket_thuc,
-sum(dich_vu_di_kem.gia*hop_dong_chi_tiet.so_luong + dich_vu.chi_phi_thue) as tong_tien from khach_hang
+sum(dich_vu.chi_phi_thue+(hop_dong_chi_tiet.so_luong*dich_vu_di_kem.gia)) as tong_tien from khach_hang
 left join loai_khach on khach_hang.ma_loai_khach = loai_khach.ma_loai_khach
 left join hop_dong on khach_hang.ma_khach_hang = hop_dong.ma_khach_hang
 left join dich_vu on hop_dong.ma_dich_vu = dich_vu.ma_dich_vu
@@ -276,7 +274,7 @@ group by thang order by thang;
 
 -- 10.	Hiển thị thông tin tương ứng với từng hợp đồng thì đã sử dụng bao nhiêu dịch vụ đi kèm.
 -- Kết quả hiển thị bao gồm ma_hop_dong, ngay_lam_hop_dong, ngay_ket_thuc, tien_dat_coc, so_luong_dich_vu_di_kem (được tính dựa trên việc sum so_luong ở dich_vu_di_kem).
-select hop_dong.ma_hop_dong, hop_dong.ngay_lam_hop_dong, hop_dong.ngay_ket_thuc, hop_dong.tien_dat_coc, count(hop_dong_chi_tiet.ma_hop_dong_chi_tiet) as so_luong_dich_vu_di_kem from hop_dong
+select hop_dong.ma_hop_dong, hop_dong.ngay_lam_hop_dong, hop_dong.ngay_ket_thuc, hop_dong.tien_dat_coc, sum(hop_dong_chi_tiet.so_luong) as so_luong_dich_vu_di_kem from hop_dong
 left join hop_dong_chi_tiet on hop_dong.ma_hop_dong = hop_dong_chi_tiet.ma_hop_dong
 group by hop_dong.ma_hop_dong;
 
@@ -298,4 +296,24 @@ inner join hop_dong_chi_tiet on hop_dong.ma_hop_dong = hop_dong_chi_tiet.ma_hop_
 where  exists(select hop_dong.ma_hop_dong where hop_dong.ngay_lam_hop_dong between '2020-10-01' and '2020-12-31')
 and not exists( select hop_dong.ma_hop_dong where hop_dong.ngay_lam_hop_dong between '2021-01-01' and '2021-06-31')
 group by dich_vu.ten_dich_vu;
+
+-- 13.	Hiển thị thông tin các Dịch vụ đi kèm được sử dụng nhiều nhất bởi các Khách hàng đã đặt phòng. (Lưu ý là có thể có nhiều dịch vụ có số lần sử dụng nhiều như nhau).
+select dich_vu_di_kem.ma_dich_vu_di_kem, dich_vu_di_kem.ten_dich_vu_di_kem,sum(hop_dong_chi_tiet.so_luong) as 'so_luong_dich_vu_di_kem'
+from dich_vu_di_kem 
+inner join hop_dong_chi_tiet on hop_dong_chi_tiet.ma_dich_vu_di_kem =dich_vu_di_kem.ma_dich_vu_di_kem 
+group by dich_vu_di_kem.ten_dich_vu_di_kem
+order by so_luong_dich_vu_di_kem desc ;
+
+-- 14.	Hiển thị thông tin tất cả các Dịch vụ đi kèm chỉ mới được sử dụng một lần duy nhất.
+-- Thông tin hiển thị bao gồm ma_hop_dong, ten_loai_dich_vu, ten_dich_vu_di_kem, so_lan_su_dung (được tính dựa trên việc count các ma_dich_vu_di_kem).
+select hop_dong.ma_hop_dong, loai_dich_vu.ten_loai_dich_vu, dich_vu_di_kem.ten_dich_vu_di_kem, count(hop_dong_chi_tiet.ma_dich_vu_di_kem) as so_lan_su_dung
+from hop_dong
+inner join hop_dong_chi_tiet as hop_dong_chi_tiet on hop_dong.ma_hop_dong=hop_dong_chi_tiet.ma_hop_dong
+inner join dich_vu_di_kem on hop_dong_chi_tiet.ma_dich_vu_di_kem=dich_vu_di_kem.ma_dich_vu_di_kem
+inner join dich_vu on hop_dong.ma_dich_vu=dich_vu.ma_dich_vu
+inner join loai_dich_vu on dich_vu.ma_loai_dich_vu=loai_dich_vu.ma_loai_dich_vu
+group by hop_dong_chi_tiet.ma_dich_vu_di_kem
+having so_lan_su_dung=1 order by hop_dong.ma_hop_dong;
+
+
 
